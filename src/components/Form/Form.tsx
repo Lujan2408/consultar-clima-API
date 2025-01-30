6c1c5f9818e09ddc23af1c5f5031bc32b5579c83
Jose David <lujan9495@gmail.com>
2025-01-30 12:03:37 -0500

    Adding css into the forms

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,14 +1,15 @@
 import { countries } from "../../data/countries"
+import styles from "./Form.module.css"
 
 export default function Form() {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
         <input type="text" id="city" placeholder="Ciudad"/>
       </div>
     
-      <div>
+      <div className={styles.field}>
         <label>País</label>
         <select>
           <option value="">-- Seleccione un País --</option>
@@ -23,7 +24,7 @@ export default function Form() {
         </select>
       </div>
     
-      <input type="submit" value="Consultar Clima" />
+      <input className={styles.submit} type="submit" value="Consultar Clima" />
     </form>
   )
 }
