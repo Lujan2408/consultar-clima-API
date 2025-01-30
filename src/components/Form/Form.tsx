a328b5679547fabbbc5de28c78c7a39ea4f55647
Jose David <lujan9495@gmail.com>
2025-01-30 12:43:24 -0500

    Placing in the state the city and country to consult the weather

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,43 @@
+import { ChangeEvent, useState } from "react"
 import { countries } from "../../data/countries"
 import styles from "./Form.module.css"
+import { SearchType } from "../../types"
 
 export default function Form() {
+
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: ''
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    setSearch({
+      ...search,
+      [e.target.id] : e.target.value
+    })
+  }
+
   return (
     <form className={styles.form}>
+
       <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
-        <input type="text" id="city" placeholder="Ciudad"/>
+        <input 
+          type="text" 
+          id="city"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
       </div>
     
       <div className={styles.field}>
-        <label>País</label>
-        <select>
+        <label htmlFor="country">País</label>
+        <select 
+          id="country"
+          value={search.country}
+          onChange={handleChange}
+        >
           <option value="">-- Seleccione un País --</option>
           {countries.map( country => (
             <option 
@@ -25,6 +51,7 @@ export default function Form() {
       </div>
     
       <input className={styles.submit} type="submit" value="Consultar Clima" />
+    
     </form>
   )
 }
