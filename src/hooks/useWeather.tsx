0023c6b4da76238698e4c3e53d3299976b2e431e
Jose David <lujan9495@gmail.com>
2025-03-22 18:13:53 -0500

    Second call to the API

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -4,15 +4,21 @@ import { SearchType } from "../types";
 export default function useWeather() {
   const fetchWeather = async (search : SearchType) => {
 
-    const appId = '5fa47e6f005d29c26292d0468039f869';
+    const appId = import.meta.env.VITE_API_KEY
     
     try {
       
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
       // Destructuring para acceder directamente a data
       const {data} = await axios.get(geoUrl)
-      console.log(data)
-    
+
+      // Acceder a la longitud y latitud
+      const lat = data[0].lat
+      const lon = data[0].lon
+      
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+      const {data: weatherResult} = await axios(weatherUrl)
+
     } catch (error) {
       console.log(error)
     }
