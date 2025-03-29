77b71e23a4b2a70160201f7a0fbba1057c613fe3
Jose David <lujan9495@gmail.com>
2025-03-29 14:56:24 -0500

    Changed URL API to use HTTPS instead of HTTP

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -41,7 +41,7 @@ export default function useWeather() {
     
     try {
       
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
       
       // Destructuring para acceder directamente a data
       const {data} = await axios.get(geoUrl)
